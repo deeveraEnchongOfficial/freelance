a08215b6fb12e87b447e163e84139cb68086875b
deeveraEnchongOfficial <64819175+deeveraEnchongOfficial@users.noreply.github.com>
2021-09-27 08:49:36 +0800

    freelance

---
+++ database/db_freelance.sql
@@ -0,0 +1,240 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2021 at 04:00 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_freelance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `meta_field` text DEFAULT NULL,
+  `meta_value` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`meta_field`, `meta_value`) VALUES
+('mobile', '09307248329'),
+('email', 'deevera.enchong18@gmail.com'),
+('facebook', 'https://facebook.com'),
+('twitter', ''),
+('linkin', ''),
+('address', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `id` int(30) NOT NULL,
+  `school` text DEFAULT NULL,
+  `degree` text DEFAULT NULL,
+  `month` varchar(50) NOT NULL,
+  `year` int(10) NOT NULL,
+  `description` text DEFAULT NULL,
+  `order_by` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `education`
+--
+
+INSERT INTO `education` (`id`, `school`, `degree`, `month`, `year`, `description`, `order_by`) VALUES
+(1, 'TES', 'Elementary', 'March', 2012, '&lt;p&gt;&lt;span style=&quot;color: rgb(34, 34, 34); font-family: consolas, &amp;quot;lucida console&amp;quot;, &amp;quot;courier new&amp;quot;, monospace; font-size: 12px; white-space: pre-wrap;&quot;&gt;Have you enjoyed your elementary life? If you ask me, yes! because in my elementary years it was so joyful and challenging despite of the difficulties .&lt;/span&gt;&lt;/p&gt;', 0),
+(2, 'THHS', 'CSS', 'April', 2018, '&lt;p&gt;&lt;span style=&quot;color: rgb(39, 40, 41); font-family: &amp;quot;Open Sans&amp;quot;, sans-serif; font-size: 16px;&quot;&gt;I am ambitious and driven. I thrive on challenge and constantly set goals for my self, so i have something to strive towards. I\'m not comfortable with setting, and I\'m always looking for an opportunity to do better and achieve greatness.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;', 0),
+(3, 'PSU', 'Information Technology', 'June', 2021, '&lt;p&gt;&lt;span style=&quot;color: rgb(39, 40, 41); font-family: &amp;quot;Open Sans&amp;quot;, sans-serif; font-size: 16px;&quot;&gt;I am ambitious and driven. I thrive on challenge and constantly set goals for my self, so i have something to strive towards. I\'m not comfortable with setting, and I\'m always looking for an opportunity to do better and achieve greatness.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `id` int(30) NOT NULL,
+  `name` text DEFAULT NULL,
+  `summary` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `banner` text DEFAULT NULL,
+  `client` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`id`, `name`, `summary`, `description`, `banner`, `client`) VALUES
+(3, 'sampleCode', 'This is the samary', '&lt;p&gt;fsagfsradgfwaesifoiahjoeifioajsdfjoiwaeofjowejf&lt;/p&gt;', 'uploads/1619229000_Code.jpg', 'mySelf'),
+(4, 'dhfdhdfh', 'dfhrfdstghfdhfdg', 'Whahahahhahahahahhahahahhhhhhhhhhhahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahahahahhahahahahhahahah', 'uploads/1619229060_PHP.jpg', 'fdhfdh'),
+(5, 'weegtearsgfdgfdg', 'gdfsgsfdgsdfgfdgdfgdfge', '&lt;p&gt;rsgsdfgsfdgsdfg34yretshgyrdfsvt5hgyre5sfhgy5er4dygh5rt4hyr5thyrt&lt;/p&gt;', 'uploads/1619229060_IT Project Management.jpg', 'dfsgdsfgsfd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Freelance/Portfolio Website Creator'),
+(2, 'address', 'Philippines'),
+(3, 'contact', '+1234567890'),
+(4, 'email', 'info@sample.com'),
+(6, 'short_name', 'My Website'),
+(9, 'logo', 'uploads/1616118180_aclc.jpg'),
+(11, 'welcome_message', 'I\'m a Web Developer creating awesome and effective Web Applications for companies of all sizes around the globe. Let\'s start scrolling and learn more about me.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `date_added`, `date_updated`) VALUES
+(1, 'Enchong', 'Dee Vera', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1619228700_profile.jpg', NULL, '2021-01-20 14:02:37', '2021-04-24 09:45:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work`
+--
+
+CREATE TABLE `work` (
+  `id` int(30) NOT NULL,
+  `company` text DEFAULT NULL,
+  `position` text DEFAULT NULL,
+  `started` varchar(250) NOT NULL,
+  `ended` varchar(250) NOT NULL,
+  `description` text DEFAULT NULL,
+  `order_by` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `work`
+--
+
+INSERT INTO `work` (`id`, `company`, `position`, `started`, `ended`, `description`, `order_by`) VALUES
+(1, 'Bitstop', 'Technician', 'October_2017', 'January_2018', '&lt;p&gt;&lt;span style=&quot;color: rgb(67, 80, 89); font-family: &amp;quot;Source Serif Pro&amp;quot;, serif; font-size: 20px; letter-spacing: -0.1px; white-space: break-spaces;&quot;&gt;I Believe a Computer Technician is a good career for me because I have been around computers&lt;/span&gt;&lt;br&gt;&lt;/p&gt;', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `work`
+--
+ALTER TABLE `work`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `education`
+--
+ALTER TABLE `education`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `work`
+--
+ALTER TABLE `work`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
