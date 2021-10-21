1fe50a8d3cbd4da2b032d18c16c268715983186d
Enchong <64819175+deeveraEnchongOfficial@users.noreply.github.com>
2021-10-21 22:01:00 +0800

    Add files via upload

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -60,7 +60,9 @@
         <!-- <a href=""></a> -->
         </strong>
         All rights reserved.
-        
+        <div class="float-right d-none d-sm-inline-block">
+          <b><?php echo $_settings->info('short_name') ?> (by: <a href="mailto:oretnom23@gmail.com" target="blank">oretnom23</a> )</b> v1.0
+        </div>
       </footer>
     </div>
     <!-- ./wrapper -->
